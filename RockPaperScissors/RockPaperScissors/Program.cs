7e3c8c8e5ada1f49bc4ac4bcd0f01e5b0b4d8c27
MUNROE <319681@dadeschools.net>
2018-03-12 12:02:03 -0400

    starting project

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 319681
+ * Date: 3/12/2018
+ * Time: 11:54 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RockPaperScissors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
